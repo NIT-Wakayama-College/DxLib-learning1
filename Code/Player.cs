51e34d27c8f56356b8bd38e0c67def9028a39950
jumpei okazaki <pokegames1578@gmail.com>
2025-03-10 19:09:48 +0900

    Update Player.cs

--- Code/Player.cs
+++ Code/Player.cs
@@ -1,16 +1,16 @@
 ﻿class Player {
-    int PlayerX = 0, PlayerY = 0;
+    int _playerX = 0, _playerY = 0;
     const int SPEED = 3;
     const int SIZE = 32;
 
     public void Update(InputState input) {
-        if (input.Up) PlayerY -= SPEED;
-        if (input.Down) PlayerY += SPEED;
-        if (input.Left) PlayerX -= SPEED;
-        if (input.Right) PlayerX += SPEED;
+        if (input.Up) _playerY -= SPEED;
+        if (input.Down) _playerY += SPEED;
+        if (input.Left) _playerX -= SPEED;
+        if (input.Right) _playerX += SPEED;
     }
 
     public void Render() {
-        Program.DrawBox(PlayerX, PlayerY, SIZE, SIZE, Program.COLOR_WHITE);
+        Program.DrawBox(_playerX, _playerY, SIZE, SIZE, Program.COLOR_WHITE);
     }
 }
