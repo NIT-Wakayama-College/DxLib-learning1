ee4d4740fd62565fd8d6a5278560a138f25a3cb3
jumpei okazaki <pokegames1578@gmail.com>
2025-03-10 15:14:08 +0900

    Refactor: ゲーム処理の責務を整理し、拡張性を向上

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,23 +1,32 @@
 ﻿using DxLibDLL;
 
-class Program {
-    static void Main(string[] args) {
+static class Game {
+    public static void Run() {
+        // 画面をクリア
+        DX.ClearDrawScreen();
+        // 文字を描画
+        DX.DrawString(100, 100, "Hello World", DX.GetColor(255, 255, 255));
+        // 裏画面の内容を表画面に反映
+        DX.ScreenFlip();
+    }
+}
+
+static class Program {
+    static void Init() {
         // ウィンドウモードで起動するように設定
         DX.ChangeWindowMode(DX.TRUE);
-
         // Dxlib の初期化
         DX.DxLib_Init();
         // 描画先を裏画面に設定
         DX.SetDrawScreen(DX.DX_SCREEN_BACK);
+    }
+
+    static void Main(string[] args) {
+        Init();
 
         // メインループ
         while (DX.ProcessMessage() == 0) {
-            // 画面をクリア
-            DX.ClearDrawScreen();
-            // 文字を描画
-            DX.DrawString(100, 100, "Hello World", DX.GetColor(255, 255, 255));
-            // 裏画面の内容を表画面に反映
-            DX.ScreenFlip();
+            Game.Run();
         }
 
         // Dxlib の終了処理
