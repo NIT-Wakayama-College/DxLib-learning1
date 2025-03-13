0e5959afb0b4e52203af0b66464e8ab9e07ccc65
jumpei okazaki <pokegames1578@gmail.com>
2025-03-14 01:17:17 +0900

    基本的なゲームループの実装

---
+++ Code/Game.cs
@@ -0,0 +1,15 @@
+﻿using DxLibDLL;
+
+internal class Game {
+    public void Run() {
+        while (DX.ProcessMessage() == 0) {
+            Render();
+        }
+    }
+
+    private void Render() {
+        DX.ClearDrawScreen();
+        DX.DrawString(100, 100, "Hello World", DX.GetColor(255, 255, 255));
+        DX.ScreenFlip();
+    }
+}
